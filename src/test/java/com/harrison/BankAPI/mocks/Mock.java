07ff76cd3ef7f11ccaa2521262d5366d3eb089c3
8harrison <monteiro4100@gmail.com>
2024-01-25 18:51:41 -0300

    Refatoração

--- src/test/java/com/harrison/BankAPI/mocks/Mock.java
+++ src/test/java/com/harrison/BankAPI/mocks/Mock.java
@@ -2,7 +2,7 @@ package com.harrison.BankAPI.mocks;
 
 public class Mock {
 
-  public static Person personMock() {
+  public static Person personMock1() {
     Person person = new Person();
     person.setName("Moacir Pitagoras");
     person.setCpf("12345678");
@@ -13,9 +13,49 @@ public class Mock {
     return person;
   }
 
-  public static Account accountMock() {
+  public static Person personMock2() {
+    Person person = new Person();
+    person.setName("Calice de Jesus");
+    person.setCpf("45454448866");
+    person.setEmail("calice.jesus@hotmail.com");
+    person.setUsername("calicejesus");
+    person.setPassword("123456");
+    person.setRole("CLIENT");
+    return person;
+  }
+
+  public static Person personMock3() {
+    Person person = new Person();
+    person.setName("Juraci Candongas");
+    person.setCpf("77777777777");
+    person.setEmail("juraci.candangas@hotmail.com");
+    person.setUsername("juracicandongas");
+    person.setPassword("777777");
+    person.setRole("CLIENT");
+    return person;
+  }
+
+  public static Person personAdminMock() {
+    Person person = new Person();
+    person.setName("Janio Quadros");
+    person.setCpf("65478123");
+    person.setEmail("janio.quadros@hotmail.com");
+    person.setUsername("janioQua");
+    person.setPassword("789654");
+    person.setRole("ADMIN");
+    return person;
+  }
+
+  public static Account accountMock1() {
     Account account = new Account();
-    account.setPerson(personMock());
+    account.setPerson(personMock1());
+    account.setSaldo(1000.00);
+    return account;
+  }
+
+  public static Account accountMock2() {
+    Account account = new Account();
+    account.setPerson(personMock2());
     account.setSaldo(1000.00);
     return account;
   }
@@ -24,7 +64,7 @@ public class Mock {
     Transaction transaction = new Transaction();
     transaction.setValor(1500.00);
     transaction.SetName("Deposito");
-    transaction.setAccount(accountMock());
+    transaction.getAccounts().put("conta", accountMock1());
     return transaction;
   }
 
@@ -32,7 +72,25 @@ public class Mock {
     Transaction transaction = new Transaction();
     transaction.setValor(500.00);
     transaction.SetName("Saque");
-    transaction.setAccount(accountMock());
+    transaction.getAccounts().put("conta", accountMock1());
+    return transaction;
+  }
+
+  public static Transaction transactionMock3() {
+    Transaction transaction = new Transaction();
+    transaction.setValor(500.00);
+    transaction.SetName("Transferência");
+    transaction.getAccounts().put("contaDe", accountMock1());
+    transaction.getAccounts().put("contaPara", accountMock2());
+    return transaction;
+  }
+
+  public static Transaction transactionMock4() {
+    Transaction transaction = new Transaction();
+    transaction.setValor(500.00);
+    transaction.SetName("PIX");
+    transaction.getAccounts().put("contaDe", accountMock1());
+    transaction.getAccounts().put("contaPara", accountMock2());
     return transaction;
   }
 }
