d99ee255e30f6a5f098915aacfe55742f20f0ffd
8harrison <monteiro4100@gmail.com>
2024-01-25 18:51:10 -0300

    Criação da classe auxiliar MockGen

--- src/test/java/com/harrison/BankAPI/mocks/MockGen.java
+++ src/test/java/com/harrison/BankAPI/mocks/MockGen.java
@@ -1,5 +1,20 @@
 package com.harrison.BankAPI.mocks;
 
-public class MockGen {
+import java.util.HashMap;
+import java.util.Map;
 
+public class MockGen extends HashMap<String, Object> {
+
+
+  public <K, V> MockGen() {
+    super();
+  }
+
+  public <K, V> MockGen(Map<K, V> source) {
+    super((Map<String, Object>) source);
+  }
+
+  public MockGen clone() {
+    return new MockGen(this);
+  }
 }
