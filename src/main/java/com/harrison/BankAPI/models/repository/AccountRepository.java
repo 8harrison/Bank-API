16714e8e93af95a7cb1b9c50dd08d81f66a8e4ae
8harrison <monteiro4100@gmail.com>
2024-01-25 23:08:17 -0300

    Implementação da interface AccountRepository

--- src/main/java/com/harrison/BankAPI/models/repository/AccountRepository.java
+++ src/main/java/com/harrison/BankAPI/models/repository/AccountRepository.java
@@ -1,5 +1,12 @@
 package com.harrison.BankAPI.models.repository;
 
-public interface AccountRepository {
+import com.harrison.BankAPI.models.entity.Account;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+  Optional<Account> findByCode(String code);
 }
