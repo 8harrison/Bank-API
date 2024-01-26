becfcc98ab9553293a239fa8d2b7dea4f5a3779c
8harrison <monteiro4100@gmail.com>
2024-01-25 23:10:20 -0300

    Implementação da classe Address

--- src/main/java/com/harrison/BankAPI/utils/Address.java
+++ src/main/java/com/harrison/BankAPI/utils/Address.java
@@ -2,4 +2,42 @@ package com.harrison.BankAPI.utils;
 
 public class Address {
 
+  private String rua;
+
+  private Integer numero;
+
+  private String cep;
+
+  public Address() {
+  }
+
+  public Address(String rua, Integer numero, String cep) {
+    this.rua = rua;
+    this.numero = numero;
+    this.cep = cep;
+  }
+
+  public String getRua() {
+    return rua;
+  }
+
+  public void setRua(String rua) {
+    this.rua = rua;
+  }
+
+  public Integer getNumero() {
+    return numero;
+  }
+
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
+
+  public String getCep() {
+    return cep;
+  }
+
+  public void setCep(String cep) {
+    this.cep = cep;
+  }
 }
