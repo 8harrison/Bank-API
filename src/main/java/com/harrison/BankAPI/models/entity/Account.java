e4518e699fd2622787366d5369d9ac632aa1cf47
8harrison <monteiro4100@gmail.com>
2024-01-25 23:07:36 -0300

    Implementação da classe entity Account

--- src/main/java/com/harrison/BankAPI/models/entity/Account.java
+++ src/main/java/com/harrison/BankAPI/models/entity/Account.java
@@ -1,5 +1,104 @@
 package com.harrison.BankAPI.models.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "accounts")
 public class Account {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(mappedBy = "account")
+  private Person person;
+
+  @OneToMany(mappedBy = "accounts")
+  private Branch branch;
+
+  private String name;
+
+  private Double saldo;
+
+  @Column(unique = true)
+  private String code;
+
+  @OneToMany(mappedBy = "account")
+  private List<Transaction> transactions;
+
+  public Account() {
+  }
+
+  public Account(Long id, Person person, Branch branch, String name, Double saldo, String code) {
+    this.id = id;
+    this.person = person;
+    this.branch = branch;
+    this.name = name;
+    this.saldo = saldo;
+    this.code = code;
+  }
+
+  public List<Transaction> getTransactions() {
+    return transactions;
+  }
+
+  public void setTransactions(List<Transaction> transactions) {
+    this.transactions = transactions;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Person getPerson() {
+    return person;
+  }
+
+  public void setPerson(Person person) {
+    this.person = person;
+  }
+
+  public Branch getBranch() {
+    return branch;
+  }
+
+  public void setBranch(Branch branch) {
+    this.branch = branch;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(Double saldo) {
+    this.saldo = saldo;
+  }
 }
