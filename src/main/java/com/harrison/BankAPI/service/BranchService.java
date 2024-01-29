d8b3ac8e7543630002504ff0b57a1737492dd5ea
8harrison <monteiro4100@gmail.com>
2024-01-29 09:56:00 -0300

    Implementação da classe BranchService

--- src/main/java/com/harrison/BankAPI/service/BranchService.java
+++ src/main/java/com/harrison/BankAPI/service/BranchService.java
@@ -2,11 +2,15 @@ package com.harrison.BankAPI.service;
 
 import com.harrison.BankAPI.exception.NotFoundException;
 import com.harrison.BankAPI.models.entity.Branch;
+import com.harrison.BankAPI.models.entity.Person;
 import com.harrison.BankAPI.models.repository.BranchRepository;
-import com.harrison.BankAPI.utils.Address;
+import com.harrison.BankAPI.models.entity.Address;
+import com.harrison.BankAPI.models.repository.PersonRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,31 +18,36 @@ public class BranchService {
 
   private final BranchRepository branchRepository;
 
+  private final PersonRepository personRepository;
+
   @Autowired
-  public BranchService(BranchRepository branchRepository) {
+  public BranchService(BranchRepository branchRepository, PersonRepository personRepository) {
     this.branchRepository = branchRepository;
+    this.personRepository = personRepository;
   }
 
   public Branch create(Branch branch) {
-    Branch created = branchRepository.save(branch);
-    created.setCode(generateCode(created.getId()));
-    return created;
+    branch.setCode(generateCode());
+    return branchRepository.save(branch);
+  }
+
+  private Person verifyManager() {
+    Optional<Person> diretor = personRepository.findByRole("MANAGER");
+    if (diretor.isEmpty()) {
+      throw new NotFoundException("Manager não encontrado!");
+    }
+    return diretor.get();
   }
 
   public Branch getById(Long id) {
-    Optional<Branch> branch = branchRepository.findById(id);
-
-    if (branch.isEmpty()) {
-      throw new NotFoundException("Agência não encontrada!");
-    }
-    return branch.get();
+   return verifyBranch(id);
   }
 
   public Branch getByCode(String code) {
     Optional<Branch> branch = branchRepository.findByCode(code);
 
     if (branch.isEmpty()) {
-      throw new NotFoundException("Agência não encontrada!");
+      throw new NotFoundException("Agência de código %s não encontrada!".formatted(code));
     }
     return branch.get();
   }
@@ -48,39 +57,34 @@ public class BranchService {
   }
 
   public Branch update(Long id, Branch branch) {
-    Optional<Branch> founded = branchRepository.findById(id);
-
-    if (founded.isEmpty()) {
-      throw new NotFoundException("Agência não encontrada!");
-    }
-    branch.setId(id);
-
+    Branch founded = verifyBranch(id);
+    branch.setId(id); branch.setCode(founded.getCode());
     return branchRepository.save(branch);
   }
 
   public String delete(Long id) {
-    Optional<Branch> branch = branchRepository.findById(id);
-
-    if (branch.isEmpty()) {
-      throw new NotFoundException("Agência não encontrada!");
-    }
+   verifyBranch(id);
     branchRepository.deleteById(id);
     return "Agência excluída com sucesso!";
   }
 
   public Branch setAddress(Long id, Address address) {
-    Optional<Branch> branch = branchRepository.findById(id);
-
-    if (branch.isEmpty()) {
-      throw new NotFoundException("Agência não encontrada!");
-    }
-    branch.get().setAddress(address);
-
-    return branchRepository.save(branch.get());
+    Branch branch = verifyBranch(id);
+    branch.setAddress(address);
+    return branchRepository.save(branch);
   }
 
-  public String generateCode(Long id) {
-    String code = "" + id;
+  private Branch verifyBranch(Long id) {
+    Optional<Branch> branch = branchRepository.findById(id);
+    if (branch.isEmpty()) {
+      throw new NotFoundException("Agência de id %s não encontrada!".formatted(id));
+    }
+    return branch.get();
+  }
+
+  public String generateCode() {
+    List<Branch> branches = branchRepository.findAll();
+    String code = "" + branches.size() + 1;
     while (code.length() < 4) {
       code = "0" + code;
     }
