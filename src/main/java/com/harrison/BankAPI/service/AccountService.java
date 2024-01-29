859f78cb5dd3a231e06d2b8b3a96e62eb157b162
8harrison <monteiro4100@gmail.com>
2024-01-29 09:56:51 -0300

    Implementação da classe AccountService

--- src/main/java/com/harrison/BankAPI/service/AccountService.java
+++ src/main/java/com/harrison/BankAPI/service/AccountService.java
@@ -1,5 +1,178 @@
 package com.harrison.BankAPI.service;
 
+import static com.harrison.BankAPI.utils.Calculator.identifier;
+
+import com.harrison.BankAPI.exception.NotFoundException;
+import com.harrison.BankAPI.models.entity.Account;
+import com.harrison.BankAPI.models.entity.Branch;
+import com.harrison.BankAPI.models.entity.Person;
+import com.harrison.BankAPI.models.entity.Transaction;
+import com.harrison.BankAPI.models.repository.AccountRepository;
+import com.harrison.BankAPI.models.repository.BranchRepository;
+import com.harrison.BankAPI.models.repository.PersonRepository;
+import com.harrison.BankAPI.models.repository.TransactionRepository;
+import com.harrison.BankAPI.models.entity.Address;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+  private final AccountRepository accountRepository;
+
+  private final TransactionRepository transactionRepository;
+
+  private final BranchRepository branchRepository;
+
+  private final PersonRepository personRepository;
+
+  @Autowired
+  public AccountService(AccountRepository accountRepository,
+      TransactionRepository transactionRepository, BranchRepository branchRepository, PersonRepository personRepository) {
+    this.accountRepository = accountRepository;
+    this.transactionRepository = transactionRepository;
+    this.branchRepository = branchRepository;
+    this.personRepository = personRepository;
+  }
+
+  public Account createAccount(Account account, String branchCode) {
+    return setCode(account, branchCode);
+  }
+
+  public Account getById(Long id) {
+    return verifyAccount(id);
+  }
+
+  public Account getByCode(String code) {
+    Optional<Account> account = accountRepository.findByCode(code);
+
+    if (account.isEmpty()) {
+      throw new NotFoundException(String.format("Conta de código %s não encontrada!", code));
+    }
+    return account.get();
+  }
+
+  public Account updateAccount(Long id, Account account) {
+    verifyAccount(id);
+    account.setId(id);
+    return accountRepository.save(account);
+  }
+
+  public String deleteAccount(Long id) {
+    verifyAccount(id);
+    accountRepository.deleteById(id);
+    return "Conta excluída com sucesso!";
+  }
+
+  public Transaction createTransaction(Long id, Transaction transaction) {
+    verifyAccount(id);
+    Map<String, Account> accounts = identifier(transaction);
+    accounts.values().forEach(account -> account.getTransactions().add(transaction));
+    accounts.values().forEach(accountRepository::save);
+    return setCode(transaction);
+  }
+
+  public Transaction getTransactionById(Long accountId, Long transactionId) {
+    verifyAccount(accountId);
+    return verifyTransaction(transactionId);
+  }
+
+  public Transaction getTransactionByCode(Long accountId, String code) {
+    verifyAccount(accountId);
+    Optional<Transaction> transaction = transactionRepository.findByCode(code);
+    if (transaction.isEmpty()) {
+      throw new NotFoundException("Transação de código %s não encontrada!".formatted(code));
+    }
+    return transaction.get();
+  }
+
+  public List<Transaction> getAllTransactions(Long accountId) {
+    return verifyAccount(accountId).getTransactions();
+  }
+
+  public String deleteTransaction(Long accountId, Long transactionId) {
+    verifyAccount(accountId);
+    verifyTransaction(transactionId);
+    transactionRepository.deleteById(transactionId);
+    return "Transação excluída com sucesso!";
+  }
+
+  public Account setAddress(Long id, Address address) {
+    Account account = verifyAccount(id);
+    account.setAddress(address);
+    return accountRepository.save(account);
+  }
+
+  public Account verifyAccount(Long id) {
+    Optional<Account> account = accountRepository.findById(id);
+    if (account.isEmpty()) {
+      throw new NotFoundException(String.format("Conta de id %s não encontrada!", id));
+    }
+    return account.get();
+  }
+
+  private Person verifyPerson(Long id) {
+    Optional<Person> person = personRepository.findById(id);
+    if (person.isEmpty()) {
+      throw new NotFoundException("Pessoa de id %s não encontrada".formatted(id));
+    }
+    return person.get();
+  }
+
+  private Transaction verifyTransaction(Long id) {
+    Optional<Transaction> transaction = transactionRepository.findById(id);
+    if (transaction.isEmpty()) {
+      throw new NotFoundException("Transação de id %s não encontrada!".formatted(id));
+    }
+    return transaction.get();
+  }
+
+  private Branch verifyBranch(String code) {
+    Optional<Branch> branch = branchRepository.findByCode(code);
+    if (branch.isEmpty()) {
+      throw new NotFoundException("Agência de código %s não encontrada".formatted(code));
+    }
+    return branch.get();
+  }
+
+  private String generateCode(Account account) {
+    int num = account.getBranch().getAccounts().size() + 1;
+    String code = "" + num;
+    while (code.length() < 5) {
+      code = "0" + code;
+    }
+    return code;
+  }
+
+  private String generateCode(Transaction transaction) {
+    int num = transaction.getTitular().getTransactions().size() + 1;
+
+    String code = "" + num;
+    while (code.length() < 7) {
+      code = "0" + code;
+    }
+    return code;
+  }
+
+  private Account setCode(Account account, String branchCode) {
+    verifyPerson(account.getPerson().getId());
+    Branch branch = verifyBranch(branchCode);
+    account.setBranch(branch);
+    String code = generateCode(account);
+    code = account.getBranch().getCode() + "-" + code;
+    account.setCode(code);
+    return accountRepository.save(account);
+  }
+
+  private Transaction setCode(Transaction transaction) {
+    String code = generateCode(transaction);
+    char letter = transaction.getName().charAt(0);
+    code += code + "-" + letter;
+    transaction.setCode(code);
+    return transactionRepository.save(transaction);
+  }
 
 }
