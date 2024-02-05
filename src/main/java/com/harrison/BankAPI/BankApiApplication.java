b0231afa0dbacdc496ab37d3ed64b3a7be3a65e0
8harrison <monteiro4100@gmail.com>
2024-02-05 17:41:17 -0300

    Pequenos ajustes

--- src/main/java/com/harrison/BankAPI/BankApiApplication.java
+++ src/main/java/com/harrison/BankAPI/BankApiApplication.java
@@ -3,9 +3,7 @@ package com.harrison.BankAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -13,9 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.harrison.BankAPI.models.repository")
 @ComponentScan("com.harrison.BankAPI")
 public class BankApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BankApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BankApiApplication.class, args);
+  }
 }
